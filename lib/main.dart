72d89c81e102dd607894d84ae0e4346a28cfe0b6
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-12-18 20:38:53 +0530

    hgh

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
-KJKASJAK
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:insta_clone/src/Bloc/BottomBarNavigation/BottmNavigationProvider.dart';
+import 'package:insta_clone/src/screens/HomeScreen/HomeScreen.dart';
+
+main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => Provider(child: HomeScreen()),
+      },
+    ));
