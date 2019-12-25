5c166081a10549459ebebbcf95bc91ecf0870110
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-12-25 23:56:18 +0530

    update userDao and addUser

--- server/daos/User/UserDao.js
+++ server/daos/User/UserDao.js
@@ -5,14 +5,13 @@ const md5 = require("md5");
 class UserDao {
   // Add User
   addUser = async data => {
-    console.log(data);
-    const { email, password, username } = data;
+    const { email, signInMethod, username } = data;
 
     const image = `https://gravatar.com/avatar/${md5(email)}?d=identicon`;
     try {
       const newUser = new User({
         email,
-        password,
+        signInMethod,
         image,
         username
       });
--- server/routes/user/addUser.js
+++ server/routes/user/addUser.js
@@ -4,7 +4,6 @@ const userDao = new UserDao();
 
 const addUser = app => {
   app.post("/addUser", async (req, res) => {
-    console.log(req.body);
     try {
       return res.json(userDao.addUser(req.body));
     } catch (err) {
