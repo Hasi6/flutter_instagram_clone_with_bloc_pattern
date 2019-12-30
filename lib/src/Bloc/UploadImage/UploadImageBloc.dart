479f3277b53a7df0fc7a86f1eb65a9a1ef723666
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-12-30 20:16:45 +0530

    Upload image to firebase and get download url

--- lib/src/Bloc/UploadImage/UploadImageBloc.dart
+++ lib/src/Bloc/UploadImage/UploadImageBloc.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:rxdart/subjects.dart';
+
+StorageReference storageReference =
+    FirebaseStorage.instance.ref().child('images');
 
 class UploadPostBloc {
   final _image = BehaviorSubject<File>();
@@ -12,6 +15,15 @@ class UploadPostBloc {
 
   // CHANGE DATA
   Function(File) get changeImage => _image.sink.add;
+
+  uploadImage() async {
+    StorageUploadTask uploadTask = storageReference.putFile(_image.value);
+    await uploadTask.onComplete;
+    print('File Uploaded');
+    storageReference.getDownloadURL().then((fileURL) {
+      print(fileURL);
+    });
+  }
 
   dispose() {
     _image.close();
