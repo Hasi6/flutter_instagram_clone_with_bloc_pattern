6a8c49d06d61c28bef845b674011692046e24c7a
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2020-01-10 23:10:26 +0530

    display user images

--- lib/src/screens/HomeScreen/FeedScreen/FeedBody/FeedBody.dart
+++ lib/src/screens/HomeScreen/FeedScreen/FeedBody/FeedBody.dart
@@ -16,10 +16,32 @@ class FeedBody extends StatelessWidget {
           return ListView.builder(
             itemCount: snapshot.data.length,
             itemBuilder: (context, index) {
-              Post postModel = Post.fromJson(snapshot.data[0]);
-              User userModel = User.fromJson(snapshot.data[0]['user']);
+              Post postModel = Post.fromJson(snapshot.data[index]);
+              User userModel = User.fromJson(snapshot.data[index]['user']);
+
               return Column(
-                children: <Widget>[Text(postModel.image), Text(userModel.id)],
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          height: 40.0,
+                          width: 40.0,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: NetworkImage(userModel.image))),
+                        )
+                      ],
+                    ),
+                  )
+                ],
               );
             },
           );
